24dde71c18b471dba290dc3b1eef2896cafd1860
MercurialMune <munenecyp@gmail.com>
2018-12-19 03:02:47 +0300

    fix indentation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 //Business Logic
 $(document).ready(function() {
-
   var currentPlayer = 0;
   var turnTaking = true;
   var maxOne = 6;
@@ -12,36 +11,20 @@ $(document).ready(function() {
   var holdButton = $("#btn2").first;
   var winnerPoints = 100;
 
-  $("#btn1").click(function() {
 
+  //User Interface
+  $("#btn1").click(function() {
     if (turnTaking) {
       $(".Die-face").text("You Rolled " + go);
-
       if (go > 1) {
         current += go;
         $(".Current-score").text("Current Score : " + current);
       } else {
         current = 0;
         $(".Current-score").text("Current Score : " + current);
-        // rivalPlayer();
       }
       total += current;
       $(".Total-score").text("Total Score : " + total);
     }
   });
-  // $("#btn2").click(function() {
-  //   if (turnTaking) {
-  //     total[currentPlayer] += current;
-  //     if (winnerPoints <= total[currentPlayer]) {
-  //       $("#news1").text("You Won the Game!!!");
-  //       turnTaking = false;
-  //     } else {
-  //       // rivalPlayer();
-  //     }
-  //   }
-  // });
-  // function rivalPlayer() {
-  //   currentPlayer === 0 ? (currentPlayer = 1) : (currentPlayer = 0);
-  //   current = 0;
-  // }
 });
